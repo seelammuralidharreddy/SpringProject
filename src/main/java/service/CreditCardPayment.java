5943cef5f2dc45fcd1f05a33add33e83c1274c59
smdr <seelammuralidharreddy.com>
2024-11-08 10:24:16 -0500

    checking total payment

--- src/main/java/service/CreditCardPayment.java
+++ src/main/java/service/CreditCardPayment.java
@@ -3,6 +3,10 @@ package service;
 public class CreditCardPayment implements IPayment  {
     public String payment(double amount)
     {
+        TotalPayment   totalPayment = new TotalPayment();
+        totalPayment.testOfTotalPay();
         return "credit Card Payment";
     }
+
+
 }
